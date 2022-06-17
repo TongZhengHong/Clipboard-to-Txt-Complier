c4862d36cd1737ee2e8991f96d839dabb368a44a
Tong Zheng Hong <zhtong315@gmail.com>
2022-06-17 21:01:23 +0800

    Move to listeners folder and add ListenerController to attach listeners to all components

---
+++ main/listeners/ListenerController.java
@@ -0,0 +1,17 @@
+package main.listeners;
+
+import main.MainWindow;
+
+public class ListenerController {
+    MainWindow mainWindow;
+
+    public ListenerController(MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+
+    }
+}
