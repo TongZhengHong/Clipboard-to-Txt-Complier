95da82887f718bd57678b598592630a197006414
Tong Zheng Hong <zhtong315@gmail.com>
2022-06-19 02:02:04 +0800

    Add new function to listen for file browser item click

--- main/listeners/MyCustomListeners.java
+++ main/listeners/MyCustomListeners.java
@@ -1,7 +1,26 @@
 package main.listeners;
 
+import java.io.File;
+
 public interface MyCustomListeners {
-	void onClipBoardUpdate(String data);
-	
-	void onTreeFolderClick(String path);
+	/**
+	 * MyCustomListener callback when clipboard changes. 
+	 * Update clipboard text area with new change and autosave if needed
+	 * @param data
+	 */
+	void onClipboardUpdate(String data);
+
+	/**
+	 * Updates outputFolderTextField which will update file browser
+	 * when folder is clicked in fileBrowser
+	 * @param path
+	 */
+	void onFileBrowserItemClick(File selectedFile);
+
+	/**
+	 * Updates outputFolderTextField which will update file browser
+	 * when folder is clicked in fileBrowser
+	 * @param path
+	 */
+	void onFileBrowserFolderClick(String path);
 }
